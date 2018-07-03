f489e8f835593c21b4847f3c8122555af4540945
Vicente Silva <vicente.pspenha@gmail.com>
2018-07-02 23:51:44 -0300

    modificações em module.config

--- module/ERP/config/module.config.php
+++ module/ERP/config/module.config.php
@@ -7,7 +7,7 @@ return array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' =>array(
-                    'route' => '/',
+                    'route' => '/ERP',
                     'defaults' => array(
                         'controller' => 'ERP\Controller\Index',
                         'action' => 'index'
@@ -17,7 +17,7 @@ return array(
             
             'application' => array(
                 'type' => 'Literal',
-                'option' => array(
+                'options' => array(
                     'route' => '/ERP',
                     'defaults' => array(
                         '__NAMESPACE__' => 'ERP\Controller',
@@ -40,10 +40,43 @@ return array(
                         )
                     )
                 )
-                
-                
             )
-            
+        )
+    ),
+//    'service_manager' => array(
+//        'factories' => array(
+//            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory'
+//        )
+//    ),
+//    'translator' => array(
+//        'locale' => 'en_US',
+//        'translation_file_patterns' => array(
+//            array(
+//                'type'      => 'gettext',
+//                'base_dir'  => __DIR__ . '/../language',
+//                'pattern'   => '%s.mo'
+//            )
+//        )
+//    ),
+    'controllers' => array(
+        'invokables' => array(
+            'ERP\Controller\Index' => 'ERP\Controller\IndexController'
+        )
+    ),
+    'view_manager' => array(
+        'display_not_found_reason'  => true,
+        'display_exceptions'        => true,
+        'doctype'                   => 'HTML5',
+        'not_found_template'        => 'error/404',
+        'exception_template'        => 'error/index',
+        'template_map' => array(
+            'layout/layout'             => __DIR__ . '../../Application/view/layout/layout.phtml',
+//            'application/index/index'   => __DIR__ . '../../Application/view/application/index/index.phtml',
+            'error/404'                 => __DIR__ . '../../Application/view/error/404.phtml',
+            'error/index'                 => __DIR__ . '../../Application/view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view'
         )
     )
     
