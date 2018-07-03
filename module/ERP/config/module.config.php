7608ef0405efa5bf45b4fbc5a0199e917ccdbdfd
Tony <Tony@192.168.15.3>
2018-07-03 13:28:47 -0300

    Atualização módulo erp

--- module/ERP/config/module.config.php
+++ module/ERP/config/module.config.php
@@ -4,82 +4,65 @@ namespace ERP;
 return array(
     'router' => array(
         'routes' => array(
-            'home' => array(
+            'categoria' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' =>array(
-                    'route' => '/ERP',
+                'options' => array(
+                    'route'    => '/erp',
                     'defaults' => array(
                         'controller' => 'ERP\Controller\Index',
-                        'action' => 'index'
-                    )
-                )
-            ),
-            
-            'application' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/ERP',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'ERP\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index'
-                    )
+                        'action'     => 'index',
+                    ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type' => 'Segment',
+                        'type'    => 'Segment',
                         'options' => array(
-                            'route' => '/[:controller[/:action]]',
+                            'route'    => '[/:action[/:id]]',
                             'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'         => '\d+'
                             ),
                             'defaults' => array(
-                            )
-                        )
-                    )
-                )
-            )
-        )
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
     ),
-//    'service_manager' => array(
-//        'factories' => array(
-//            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory'
-//        )
-//    ),
-//    'translator' => array(
-//        'locale' => 'en_US',
-//        'translation_file_patterns' => array(
-//            array(
-//                'type'      => 'gettext',
-//                'base_dir'  => __DIR__ . '/../language',
-//                'pattern'   => '%s.mo'
-//            )
-//        )
-//    ),
     'controllers' => array(
         'invokables' => array(
             'ERP\Controller\Index' => 'ERP\Controller\IndexController'
-        )
+        ),
     ),
     'view_manager' => array(
-        'display_not_found_reason'  => true,
-        'display_exceptions'        => true,
-        'doctype'                   => 'HTML5',
-        'not_found_template'        => 'error/404',
-        'exception_template'        => 'error/index',
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'             => __DIR__ . '../../Application/view/layout/layout.phtml',
-//            'application/index/index'   => __DIR__ . '../../Application/view/application/index/index.phtml',
-            'error/404'                 => __DIR__ . '../../Application/view/error/404.phtml',
-            'error/index'                 => __DIR__ . '../../Application/view/error/index.phtml',
+            'layout/layout'           => __DIR__ . '/../../Application/view/layout/layout.phtml',
+            'error/404'               => __DIR__ . '/../../Application/view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../../Application/view/error/index.phtml',
         ),
         'template_path_stack' => array(
-            __DIR__ . '/../view'
-        )
-    )
-    
-    
-    
+            __DIR__ . '/../view',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ),
+            ),
+        ),
+    ),
 );
