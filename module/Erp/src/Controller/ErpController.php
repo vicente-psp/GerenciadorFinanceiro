1d31cd90c511e54920e791475285481d74c7a84b
Tony <Tony@192.168.15.3>
2018-07-04 14:01:23 -0300

    Atualização módulo erp

--- module/Erp/src/Controller/ErpController.php
+++ module/Erp/src/Controller/ErpController.php
@@ -2,14 +2,15 @@
 namespace Erp\Controller;
 
 use Application\Controller\AbstractController;
-class IndexController extends AbstractController
-{
-    function __construct()
-    {
+
+class ErpController extends AbstractController {
+
+    function __construct() {
         $this->form = 'Categoria\Form\CategoryForm';
         $this->controller = 'categoria';
         $this->route = 'categoria/default';
         $this->service = 'Categoria\Service\CategoriaService';
         $this->entity = 'Categoria\Entity\Category';
     }
+
 }
